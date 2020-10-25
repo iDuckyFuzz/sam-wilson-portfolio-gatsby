38b7deb6c4a58a7557d5297f56f0b2457a909e92
John Doe <iduckyfuzz@gmail.com>
2020-10-25 02:03:27 +0000

    updated first blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Sam Wilson - Junior Developer Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Sam Wilson`,
+      summary: `who lives and works in Nottingham building not so useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `SWils1990`,
     },
   },
   plugins: [
